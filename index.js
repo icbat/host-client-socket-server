81170f2aa401c788859c135f140684edea75ac41
Evan Cobb <cobb.evan@gmail.com>
2018-02-12 22:14:31 -0600

    basic message routing in place

--- index.js
+++ index.js
@@ -3,11 +3,37 @@ const port = 3000;
 const server = new WebSocketServer({port: port});
 console.log('Socker server running on port', port);
 
-server.on('connection', client => {
-    console.log('Someone connected', client);
-    client.on('message', message => {
-        console.log('We received', message);
-        client.send('You sent' + message);
+const createNewRoom = message => {
+    console.log('Creating new room given', message);
+};
+const joinRoom = message => {
+    console.log('Joining a room given', message);
+};
+const sendMessageToRoom = message => {
+    console.log('Sending message to your room', message);
+};
+
+const messageTypeHandlers = {
+    newRoom: createNewRoom,
+    joinRoom: joinRoom,
+    sendToMyRoom: sendMessageToRoom,
+};
+
+server.on('connection', connection => {
+    console.log('Someone connected', connection);
+    connection.on('message', rawMessage => {
+        let message;
+        try {
+            message = JSON.parse(rawMessage);
+        } catch (parseError) {
+            console.error('Sent not-JSON wut do');
+            return;
+        }
+        if (message.type in messageTypeHandlers) {
+            messageTypeHandlers[message.type](message.text);
+        } else {
+            console.error('Unexpected message type', message.type);
+            return;
+        }
     });
-    client.send('Hello, you are connected!');
 });
